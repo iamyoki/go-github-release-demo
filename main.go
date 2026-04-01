647cd9d2764cd31729974c7804dfae52e31f7c41
Yoki <74389358+iamyoki@users.noreply.github.com>
2026-04-02 01:37:25 +0800

    fix: remove redundant hello

--- main.go
+++ main.go
@@ -4,10 +4,4 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello")
-	fmt.Println("Hello2")
-	fmt.Println("Hello3")
-	fmt.Println("Hello4")
-	fmt.Println("Hello5")
-	fmt.Println("Hello6")
-	fmt.Println("Hello7")
 }
